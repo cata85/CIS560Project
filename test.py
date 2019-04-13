fa453790cf1bc977913a0fc1dd9a9b5ad2bd7aab
Carter Nelson <cartern@ksu.edu>
2019-04-12 19:46:59 -0500

    Added a test file to view the data.

---
+++ test.py
@@ -0,0 +1,16 @@
+import config
+import pymssql
+
+
+conn = pymssql.connect(
+    server=config.SERVER, 
+    user=config.USER, 
+    password=config.PASSWORD, 
+    database=config.DB
+)
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'")
+data = cursor.fetchall()
+for row in data:
+    print(row)
